6ba9a86476df2e3ef0db92a704e00b02e167ac70
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-08-01 19:01:15 +0700

    change ERC1155 rece

--- contracts/Smelter.sol
+++ contracts/Smelter.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
-import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "./IMonsterToken.sol";
 
-
-contract Smelter is ERC1155Receiver{
+contract Smelter is ERC1155Holder {
     struct Smelt {
-        uint quantity;
-        uint startTime;
+        uint256 quantity;
+        uint256 startTime;
     }
 
     IERC1155 itemInterface;
@@ -21,38 +20,24 @@ contract Smelter is ERC1155Receiver{
         tokenInterface = IMonsterToken(_monsterToken);
     }
 
-    function smelt(address _user, uint _quantity) public{
-        require(itemInterface.balanceOf(_user, 4) > 0,"You don't have a any crystal to be smelt");
-        itemInterface.safeTransferFrom(_user, address (this), 4, _quantity, "");
+    function smelt(address _user, uint256 _quantity) public {
+        require(
+            itemInterface.balanceOf(_user, 4) > 0,
+            "You don't have a any crystal to be smelt"
+        );
+        itemInterface.safeTransferFrom(_user, address(this), 4, _quantity, "");
         smeltDetails[_user] = Smelt(_quantity, block.timestamp);
     }
 
     function claimSmelt(address _user) public {
-        (uint quantity, uint start) = (smeltDetails[_user].quantity, smeltDetails[_user].startTime);
-        uint time = start + (quantity * 15 minutes);
-        uint reward = quantity * 4;
+        (uint256 quantity, uint256 start) = (
+            smeltDetails[_user].quantity,
+            smeltDetails[_user].startTime
+        );
+        uint256 time = start + (quantity * 15 minutes);
+        uint256 reward = quantity * 4;
 
         require(time < block.timestamp, "Your crystal stil being smelted");
         tokenInterface.mint(_user, reward);
-    }
-
-    function onERC1155BatchReceived(
-    address _operator,
-    address _from,
-    uint256[] calldata _ids,
-    uint256[] calldata _values,
-    bytes calldata _data
-    ) external pure override returns(bytes4) {
-        bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
-    }
-
-    function onERC1155Received (
-    address _operator,
-    address _from,
-    uint256 _id,
-    uint256 _value,
-    bytes calldata _data
-    ) external pure override returns(bytes4) {
-        bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
     }
 }
