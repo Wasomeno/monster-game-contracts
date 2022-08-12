4578d407f69f181aabacec5b2e6364ab3fc5fdec
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-08-12 23:38:04 +0700

    Add connect function

--- src/NotConnected.js
+++ src/NotConnected.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-const NotConnected = () => {
+const NotConnected = ({ account, setAccount }) => {
+  async function connectAccount() {
+    if (window.ethereum) {
+      const account = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(account);
+    }
+  }
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
+    <div className="d-flex flex-column justify-content-center align-items-center h-100 w-100 text-center">
       <h2 className="p-2 text-white">Connect Your Wallet</h2>
+      <div className="row justify-content-center align-items-center">
+        <button
+          className="btn btn-primary px-3 rounded-pill"
+          onClick={connectAccount}
+        >
+          Connect
+        </button>
+      </div>
     </div>
   );
 };
