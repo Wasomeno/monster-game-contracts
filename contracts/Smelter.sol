0b65f7161fa5015e845fb83abfe2db2c4a0b99ac
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-10 15:52:38 +0700

    Added smelting status map and modifiers

--- contracts/Smelter.sol
+++ contracts/Smelter.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "./IMonsterToken.sol";
 
 contract Smelter is ERC1155Holder {
-    struct Smelt {
+    struct Details {
         uint256 quantity;
         uint256 startTime;
     }
@@ -13,29 +13,44 @@ contract Smelter is ERC1155Holder {
     IERC1155 itemInterface;
     IMonsterToken tokenInterface;
 
-    mapping(address => Smelt) public smeltDetails;
+    mapping(address => Details) public smeltDetails;
+    mapping(address => bool) public smeltingStatus;
+
+    modifier isSmelting(address _user) {
+        bool status = smeltingStatus[_user];
+        require(status, "You're not smelting any crystals");
+        _;
+    }
+
+    modifier isNotSmelting(address _user) {
+        bool status = smeltingStatus[_user];
+        require(!status, "You're still smelting");
+        _;
+    }
 
     function setInterface(address _itemNFT, address _monsterToken) public {
         itemInterface = IERC1155(_itemNFT);
         tokenInterface = IMonsterToken(_monsterToken);
     }
 
-    function smelt(address _user, uint256 _quantity) public {
-        require(
-            itemInterface.balanceOf(_user, 4) > 0,
-            "You don't have a any crystal to be smelt"
-        );
+    function smelt(address _user, uint256 _quantity)
+        external
+        isNotSmelting(_user)
+    {
+        uint256 crystalBalance = itemInterface.balanceOf(_user, 4);
+        require(crystalBalance > 0, "You don't have a any crystal to be smelt");
+        require(_quantity >= 100, "Max quantity exceed");
         itemInterface.safeTransferFrom(_user, address(this), 4, _quantity, "");
-        smeltDetails[_user] = Smelt(_quantity, block.timestamp);
+        smeltDetails[_user] = Details(_quantity, block.timestamp);
     }
 
-    function claimSmelt(address _user) public {
+    function finishSmelting(address _user) external isSmelting(_user) {
         (uint256 quantity, uint256 start) = (
             smeltDetails[_user].quantity,
             smeltDetails[_user].startTime
         );
         uint256 time = start + (quantity * 15 minutes);
-        uint256 reward = quantity * 4;
+        uint256 reward = quantity * 5;
 
         require(time < block.timestamp, "Your crystal stil being smelted");
         tokenInterface.mint(_user, reward);
