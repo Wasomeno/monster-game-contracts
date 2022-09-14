015b34826b69136b172539510d86ef3083e72a25
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-14 23:30:10 +0700

    Added UsersData contract and interface

---
+++ contracts/UsersData.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+contract UsersData {
+    struct Details {
+        address user;
+        bytes32 profile_image;
+        bytes32 name;
+    }
+
+    address[] public users;
+    mapping(address => Details) public userDataDetails;
+    mapping(address => bool) public registrationStatus;
+
+    event Registered(address _user, bytes32 _name);
+
+    modifier isNotRegistered(address _user) {
+        bool status = registrationStatus[_user];
+        require(!status, "You're already registered");
+        _;
+    }
+
+    function register(bytes32 _name, bytes32 _profile)
+        external
+        isNotRegistered(msg.sender)
+    {
+        userDataDetails[msg.sender] = Details(msg.sender, _profile, _name);
+        emit Registered(msg.sender, _name);
+    }
+
+    function checkRegister(address _user) external view returns (bool result) {
+        bool status = registrationStatus[_user];
+        if (status != false) {
+            result = true;
+        }
+    }
+}
