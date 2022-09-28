c482681354ebfecd26adfa05c9207f4a5e69edac
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-28 20:24:14 +0700

    Added registration checker and custom errors

--- contracts/Dungeon.sol
+++ contracts/Dungeon.sol
@@ -3,33 +3,111 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./IMonster.sol";
+import "./IUsersData.sol";
 import "./IItems.sol";
 
 contract Dungeon {
     struct Details {
-        uint256[] monsters;
+        uint256 monstersAmount;
+        mapping(uint256 => uint256) monsters;
         uint256 startTime;
         address owner;
     }
 
-    IERC721 monsterInterface;
-    IMonster statsInterface;
-    IItems itemInterface;
+    IERC721 public monsterInterface;
+    IMonster public statsInterface;
+    IItems public itemInterface;
+    IUsersData public usersDataInterface;
 
-    uint256 nonce;
+    uint256 internal nonce;
     mapping(address => Details) public monstersOnDungeon;
     mapping(address => bool) public dungeoningStatus;
 
+    error NotDungeoning(bool _status);
+    error IsDungeoning(bool _status);
+    error NotValidToFinishDungeon(uint256 _timeElapsed, uint256 _timeNow);
+    error MonstersAmountNotValid(uint256 _amount, uint256 _limit);
+    error NotValidToDungeon(
+        uint256 _status,
+        uint256 _cooldown,
+        uint256 _energy
+    );
+
     modifier isDungeoning(address _user) {
         bool status = dungeoningStatus[_user];
-        require(status, "You're not dungeoning");
+        if (!status) {
+            revert NotDungeoning(status);
+        }
         _;
     }
 
     modifier isNotDungeoning(address _user) {
         bool status = dungeoningStatus[_user];
-        require(!status, "You're still dungeoning");
+        if (status) {
+            revert IsDungeoning(status);
+        }
         _;
+    }
+
+    modifier isRegistered(address _user) {
+        usersDataInterface.checkRegister(_user);
+        _;
+    }
+
+    function startDungeon(uint256[] calldata _monsters)
+        external
+        isNotDungeoning(msg.sender)
+        isRegistered(msg.sender)
+    {
+        if (_monsters.length > 6) {
+            revert MonstersAmountNotValid(_monsters.length, 6);
+        }
+        Details storage details = monstersOnDungeon[msg.sender];
+        for (uint256 i; i < _monsters.length; ++i) {
+            uint256 monster = _monsters[i];
+            uint256 level = statsInterface.getMonsterLevel(monster);
+            uint256 status = statsInterface.getMonsterStatus(monster);
+            uint256 energy = statsInterface.getMonsterEnergy(monster);
+            uint256 cooldown = statsInterface.getMonsterCooldown(monster);
+            if (status != 0 || cooldown > block.timestamp || energy < 20) {
+                revert NotValidToDungeon(status, cooldown, energy);
+            }
+            statsInterface.setStatus(monster, 3);
+            details.monsters[i] = monster;
+        }
+        details.monstersAmount = _monsters.length;
+        details.owner = msg.sender;
+        details.startTime = block.timestamp;
+        dungeoningStatus[msg.sender] = true;
+    }
+
+    function finishDungeon() external isDungeoning(msg.sender) {
+        Details storage details = monstersOnDungeon[msg.sender];
+        uint256[] memory monsters = getMonstersOnDungeon(msg.sender);
+        uint256 expEarned = 8;
+        uint256 elapsedTime = details.startTime + 30 minutes;
+        if (elapsedTime > block.timestamp) {
+            revert NotValidToFinishDungeon(elapsedTime, block.timestamp);
+        }
+        for (uint256 i; i < monsters.length; ++i) {
+            uint256 monster = monsters[i];
+            uint256 energy = statsInterface.getMonsterEnergy(monster);
+            uint256 newEnergy = energy - 20;
+            uint256 level = statsInterface.getMonsterLevel(monster);
+            uint256 odds = bossFightChance(level * 30);
+            statsInterface.setCooldown(monster);
+            statsInterface.setEnergy(monster, newEnergy);
+            statsInterface.expUp(monster, expEarned);
+            itemInterface.bossFightReward(
+                monster,
+                msg.sender,
+                randomNumber(),
+                odds
+            );
+            statsInterface.setStatus(monster, 0);
+        }
+        deleteDetails(msg.sender);
+        dungeoningStatus[msg.sender] = false;
     }
 
     function setInterface(address monsterNFT, address itemNFT) public {
@@ -38,57 +116,35 @@ contract Dungeon {
         itemInterface = IItems(itemNFT);
     }
 
-    function startDungeon(uint256[] calldata _monsters, address _user)
-        external
-        isNotDungeoning(_user)
-    {
-        require(_monsters.length <= 6, "Exceed Limit");
-        for (uint256 i; i < _monsters.length; ++i) {
-            uint256 monster = _monsters[i];
-            uint256 level = statsInterface.getMonsterLevel(monster);
-            uint256 status = statsInterface.getMonsterStatus(monster);
-            uint256 energy = statsInterface.getMonsterEnergy(monster);
-            uint256 cooldown = statsInterface.getMonsterCooldown(monster);
-            require(
-                cooldown <= block.timestamp,
-                " Your monster is on cooldown"
-            );
-            require(status == 0, "Your monster is active");
-            require(energy >= 20, "Not enough energy");
-            statsInterface.setStatus(monster, 3);
-        }
-        monstersOnDungeon[_user] = Details(_monsters, block.timestamp, _user);
-    }
-
-    function finishDungeon(address _user) external isDungeoning(_user) {
-        uint256[] memory monsters = monstersOnDungeon[_user].monsters;
-        for (uint256 i; i < monsters.length; ++i) {
-            uint256 monster = monsters[i];
-            uint256 energy = statsInterface.getMonsterEnergy(monster);
-            uint256 newEnergy = energy - 20;
-            uint256 expEarned = 8;
-            uint256 level = statsInterface.getMonsterLevel(monster);
-            uint256 odds = bossFightChance(level * 30);
-            statsInterface.setCooldown(monster);
-            statsInterface.setEnergy(monster, newEnergy);
-            statsInterface.expUp(monster, expEarned);
-            itemInterface.bossFightReward(monster, _user, randomNumber(), odds);
-            statsInterface.setStatus(monster, 0);
-        }
-        deleteUserDetails(_user);
-    }
-
-    function deleteUserDetails(address _user) internal {
-        Details memory details = monstersOnDungeon[_user];
-        delete details;
-    }
-
     function getMonstersOnDungeon(address _user)
-        external
+        public
         view
         returns (uint256[] memory _monsters)
     {
-        _monsters = monstersOnDungeon[_user].monsters;
+        Details storage details = monstersOnDungeon[_user];
+        uint256 amount = details.monstersAmount;
+        _monsters = new uint256[](amount);
+        for (uint256 i; i < amount; ++i) {
+            uint256 monster = details.monsters[i];
+            _monsters[i] = monster;
+        }
+    }
+
+    function deleteDetails(address _user) internal {
+        Details storage details = monstersOnDungeon[_user];
+        delete details.monstersAmount;
+        delete details.owner;
+        delete details.startTime;
+        deleteMonsters(_user);
+    }
+
+    function deleteMonsters(address _user) internal {
+        Details storage details = monstersOnDungeon[_user];
+        uint256 amount = details.monstersAmount;
+        for (uint256 i; i < amount; ++i) {
+            uint256 monster = details.monsters[i];
+            delete monster;
+        }
     }
 
     function randomNumber() internal returns (uint256) {
