91ba5068a09e0e455319488b2cf1ceaa4768bfaa
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-09-30 21:14:41 +0700

    Changed checkregister non returns

--- contracts/IUsersData.sol
+++ contracts/IUsersData.sol
@@ -2,5 +2,5 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 interface IUsersData {
-    function checkRegister(address _user) external view returns (bool result);
+    function checkRegister(address _user) external view;
 }
